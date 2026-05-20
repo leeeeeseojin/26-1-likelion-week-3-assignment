6f4a28f334d8ccf84217917448bd6b41944198db
leeeeeseojin <kkyuuu00@gmail.com>
2026-05-20 13:38:48 +0900

    feat: add formatMovie util to convert TMDB response

--- src/pages/mainPage/utils/formatMovie.js
+++ src/pages/mainPage/utils/formatMovie.js
@@ -1,6 +1,12 @@
+// TMDB API 응답 형식을 사용하고자하는 형식으로 변환
+// title -> title
+// overview -> description
+// release_date ->  releaseDate
+// vote_average -> rating
+// poster_path -> thumbnail
+// genre_ids[0] -> genr (이름으로 변환)
 const IMAGE_BASE_URL = import.meta.env.VITE_TMDB_IMAGE_BASE_URL;
 
-// TMDB 응답 데이터를 MovieCard에서 사용하는 형식으로 변환
 export const formatMovie = (movie, genres = []) => {
   const genreName =
     genres.find((g) => g.id === movie.genre_ids?.[0])?.name ?? "기타";
@@ -10,7 +16,8 @@ export const formatMovie = (movie, genres = []) => {
     title: movie.title,
     description: movie.overview,
     releaseDate: movie.release_date,
-    rating: Math.round(movie.vote_average * 10) / 10,
+    // vote_average는 소수점 2자리 표시
+    rating: Math.round(movie.vote_average * 100) / 100,
     thumbnail: movie.poster_path
       ? `${IMAGE_BASE_URL}${movie.poster_path}`
       : null,
